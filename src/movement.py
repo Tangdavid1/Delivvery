6bc65455cd4dba01239d881462cf7c8b6d9c96ac
Santiago Padron <santi.padron@yahoo.com>
2025-11-14 14:18:45 -0500

    more movement functions, it works well-ish

--- src/movement.py
+++ src/movement.py
@@ -5,7 +5,13 @@ import math
 TOUCH_SENSOR = TouchSensor(2)
 RIGHT_MOTOR = Motor("C")
 LEFT_MOTOR = Motor("B")
+CONVEYOR = Motor("D")
+
 WHEEL_DIAMETER_CM = 4.3
+WHEEL_RADIUS_CM = WHEEL_DIAMETER_CM/2
+AXLE_LENGTH_CM = 7.5
+ORIENT_TO_DEG = AXLE_LENGTH_CM/WHEEL_RADIUS_CM
+DISTANCE_TO_DEG = 360/(math.pi*WHEEL_DIAMETER_CM)
 
 POWER_LIMIT = 100
 SPEED_LIMIT = 720
@@ -28,33 +34,82 @@ def init_motor(motor: Motor):
         print(error)
 
 
-def go_forward(distance_cm):
+def go_forward_single(motor: Motor, distance_cm: int):
     #calculate distance to move in degrees
-    deg_to_move = int(distance_cm*(360/(math.pi*WHEEL_DIAMETER_CM)))
+    deg_to_move = int(distance_cm*DISTANCE_TO_DEG)
     # move forward
     try:
-        LEFT_MOTOR.set_position_relative(deg_to_move)
-        RIGHT_MOTOR.set_position_relative(deg_to_move)
+        motor.set_position_relative(deg_to_move)
+    except IOError as error:
+        print(error)
 
-        wait_for_motor(RIGHT_MOTOR)
+def go_forward_both_wheels(left_motor: Motor, right_motor: Motor, distance_cm: int):
+    try:
+        go_forward_single(left_motor, distance_cm)
+        go_forward_single(right_motor, distance_cm)
+        wait_for_motor(right_motor)
     except IOError as error:
         print(error)
 
 
-#def turn_180():
+def turn_180(left_motor: Motor, right_motor: Motor):
+    deg_to_move = int(207*ORIENT_TO_DEG)
 
-#def turn_90_left():
+    try:
+        left_motor.set_position_relative(deg_to_move)
+        right_motor.set_position_relative(-deg_to_move)
+        wait_for_motor(right_motor)
+    except IOError as error:
+        print(error)
+
+def turn_90_left(left_motor: Motor, right_motor: Motor):
+    deg_to_move = int(-110*ORIENT_TO_DEG)
+
+    try:
+        left_motor.set_position_relative(deg_to_move)
+        right_motor.set_position_relative(-deg_to_move)
+        wait_for_motor(right_motor)
+    except IOError as error:
+        print(error)
     
-#def turn_90_right():
+def turn_90_right(left_motor: Motor, right_motor: Motor):
+    deg_to_move = int(200*ORIENT_TO_DEG)
+
+    try:
+        left_motor.set_position_relative(deg_to_move)
+#        right_motor.set_position_relative(-deg_to_move)
+        wait_for_motor(left_motor)
+    except IOError as error:
+        print(error)
 
 if __name__ == "__main__":
     try:
         print("movement test.")
         init_motor(LEFT_MOTOR)
         init_motor(RIGHT_MOTOR)
+        init_motor(CONVEYOR)
+        go_forward_single(CONVEYOR, 25)
+        wait_for_motor(CONVEYOR)
 
-        print("try to move forward")
-        go_forward(100)
+        #print("try to move forward")
+        #go_forward_both_wheels(LEFT_MOTOR, RIGHT_MOTOR, 50)
+
+        #print("try to rotate 180")
+        #turn_180(LEFT_MOTOR, RIGHT_MOTOR)
+
+        #print("try to rotate 90 left")
+        #turn_90_left(LEFT_MOTOR, RIGHT_MOTOR)
+
+        #print("try to rotate 90 right")
+        #turn_90_right(LEFT_MOTOR, RIGHT_MOTOR)
+        #go_forward_both_wheels(LEFT_MOTOR, RIGHT_MOTOR, 80)
+        #turn_90_right(LEFT_MOTOR, RIGHT_MOTOR)
+        #go_forward_both_wheels(LEFT_MOTOR, RIGHT_MOTOR, 20)
+        #turn_90_right(LEFT_MOTOR, RIGHT_MOTOR)
+        #go_forward_both_wheels(LEFT_MOTOR, RIGHT_MOTOR, 10)
+        #turn_90_right(LEFT_MOTOR, RIGHT_MOTOR)
+        #go_forward_both_wheels(LEFT_MOTOR, RIGHT_MOTOR, 80)
+        #turn_90_right(LEFT_MOTOR, RIGHT_MOTOR)
 
     except KeyboardInterrupt:
         print("Ended program")
