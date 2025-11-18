55f8b144b13d02870c8bd18c447a8c2b4ca8757a
Santiago Padron <santi.padron@yahoo.com>
2025-11-18 18:44:42 -0500

    rename obsolete files, remove unused files, update readme

--- src/office_scanning.py
+++ src/office_scanning.py
@@ -1,6 +1,6 @@
 from utils.brick import Motor, EV3ColorSensor
 
-import movement
+import movement_OBSOLETE
 import color_average_finder
 import color_detect
 import time 
@@ -31,7 +31,7 @@ def scanningProcess(motor: Motor):
     global colorScanned
 
     # turn left and scan for color
-    movement.turn_90_left(motor, motor)
+    movement_OBSOLETE.turn_90_left(motor, motor)
 
     time.sleep(1)  
     if color.get_color() != 'None':  
@@ -39,17 +39,17 @@ def scanningProcess(motor: Motor):
         return
 
     # Go back to original position by reverting turn left
-    movement.turn_angle(motor, motor, backleft_angle)
+    movement_OBSOLETE.turn_angle(motor, motor, backleft_angle)
 
     # turn right and scan for color
-    movement.turn_90_right(motor, motor)
+    movement_OBSOLETE.turn_90_right(motor, motor)
     time.sleep(1)  
     if color.get_color() != 'None':
         colorScanned = True
         return
 
     # Go back to original position by reverting turn right
-    movement.turn_angle(motor, motor, backright_angle)
+    movement_OBSOLETE.turn_angle(motor, motor, backright_angle)
 
 def scanOffice(motor: Motor):
     global colorScanned
@@ -58,19 +58,19 @@ def scanOffice(motor: Motor):
         scanningProcess(motor)
 
         # if not color scanned, move forward and scan again
-        movement.go_forward_both_wheels(motor, motor, 60)
+        movement_OBSOLETE.go_forward_both_wheels(motor, motor, 60)
         scanningProcess(motor)
 
         # if still not scanned, move backwards and scan again
-        movement.go_forward_both_wheels(motor, motor, -20)
+        movement_OBSOLETE.go_forward_both_wheels(motor, motor, -20)
         scanningProcess(motor)
 
         # if still not scanned, move backwards and scan again
-        movement.go_forward_both_wheels(motor, motor, -20)
+        movement_OBSOLETE.go_forward_both_wheels(motor, motor, -20)
         scanningProcess(motor)
 
         # if still not scanned, move forward to original position
-        movement.go_forward_both_wheels(motor, motor, -20)
+        movement_OBSOLETE.go_forward_both_wheels(motor, motor, -20)
         scanningProcess(motor)
 
 
