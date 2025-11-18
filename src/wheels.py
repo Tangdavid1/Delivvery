99b4d51b7e073b9ba5cd225802afa702bc37fbee
Santiago Padron <santi.padron@yahoo.com>
2025-11-18 14:03:58 -0500

    refactored wheel movement as a class

--- src/wheels.py
+++ src/wheels.py
@@ -4,10 +4,10 @@ import math
 
 class Wheels:
     # Initializer
-    def __init__(self, left_wheel_port, right_wheel_port):
+    def __init__(self, left_wheel_port="B", right_wheel_port="C"):
         # Initialize motors
-        self.RIGHT_MOTOR = Motor("C")
-        self.LEFT_MOTOR = Motor("B")
+        self.RIGHT_MOTOR = Motor(right_wheel_port)
+        self.LEFT_MOTOR = Motor(left_wheel_port)
         # Constant values
         self.WHEEL_DIAMETER_CM = 4.3
         self.WHEEL_RADIUS_CM = self.WHEEL_DIAMETER_CM/2
@@ -15,10 +15,23 @@ class Wheels:
         # More constants regarding motors
         self.ORIENT_TO_DEG = self.AXLE_LENGTH_CM/self.WHEEL_RADIUS_CM
         self.DISTANCE_TO_DEG = 360/(math.pi*self.WHEEL_DIAMETER_CM)
-        self.POWER_LIMIT = 100
+        self.POWER_LIMIT = 80
         self.SPEED_LIMIT = 720
         self.FWD_SPEED = 100
         self.MOTOR_POLL_DELAY = 0.05
+        # Constants for accurate movement
+        self.DEG_180_TURN = 200
+
+        try:
+            self.RIGHT_MOTOR.reset_encoder()
+            self.RIGHT_MOTOR.set_limits(self.POWER_LIMIT, self.SPEED_LIMIT)
+            self.RIGHT_MOTOR.set_power(0)
+
+            self.LEFT_MOTOR.reset_encoder()
+            self.LEFT_MOTOR.set_limits(self.POWER_LIMIT, self.SPEED_LIMIT)
+            self.LEFT_MOTOR.set_power(0)
+        except IOError as error:
+            print(error)
     
     # Method that waits for motor to complete their movement
     def wait_for_motor(self, motor: Motor):
@@ -27,78 +40,72 @@ class Wheels:
         while not math.isclose(motor.get_speed(), 0):
             time.sleep(self.MOTOR_POLL_DELAY)
     
-    # Method that initializes a motor by setting correct limits
-    def init_motor(self, motor: Motor):
-        try:
-            motor.reset_encoder()
-            motor.set_limits(self.POWER_LIMIT, self.SPEED_LIMIT)
-            motor.set_power(0)
-        except IOError as error:
-            print(error)
-    
-    def go_forward_single(self, motor: Motor, distance_cm: int):
-        #calculate distance to move in degrees
-        deg_to_move = int(distance_cm*DISTANCE_TO_DEG)
-        # move forward
-        try:
-            motor.set_position_relative(deg_to_move)
-        except IOError as error:
-            print(error)
 
-    def go_forward_both_wheels(self, left_motor: Motor, right_motor: Motor, distance_cm: int):
+    def go_straight(self, distance_cm):
+        deg_to_move = int(distance_cm*self.DISTANCE_TO_DEG)
         try:
-            go_forward_single(left_motor, distance_cm)
-            go_forward_single(right_motor, distance_cm)
-            wait_for_motor(right_motor)
+            self.LEFT_MOTOR.set_position_relative(deg_to_move)
+            self.RIGHT_MOTOR.set_position_relative(deg_to_move)
+            self.wait_for_motor(self.RIGHT_MOTOR)
         except IOError as error:
             print(error)
 
 
-    def turn_180(left_motor: Motor, right_motor: Motor):
-        deg_to_move = int(207*ORIENT_TO_DEG)
+    def turn_180(self, right_left_not: bool):
+        if right_left_not:
+            deg_to_move = int(self.DEG_180_TURN*self.ORIENT_TO_DEG)
+        else:
+            deg_to_move = int(-self.DEG_180_TURN*self.ORIENT_TO_DEG)
 
         try:
-            left_motor.set_position_relative(deg_to_move)
-            right_motor.set_position_relative(-deg_to_move)
-            wait_for_motor(right_motor)
+            self.LEFT_MOTOR.set_position_relative(deg_to_move)
+            self.RIGHT_MOTOR.set_position_relative(-deg_to_move)
+            self.wait_for_motor(self.RIGHT_MOTOR)
         except IOError as error:
             print(error)
 
-    def turn_90_left(left_motor: Motor, right_motor: Motor):
-        deg_to_move = int(200*ORIENT_TO_DEG)
+    def turn_90_left(self):
+        deg_to_move = int(self.DEG_180_TURN*self.ORIENT_TO_DEG)
 
         try:
-            right_motor.set_position_relative(deg_to_move)
-            wait_for_motor(right_motor)
+            self.RIGHT_MOTOR.set_position_relative(deg_to_move)
+            self.wait_for_motor(self.RIGHT_MOTOR)
         except IOError as error:
             print(error)
 
 
-            
-    def turn_90_right(left_motor: Motor, right_motor: Motor):
-        deg_to_move = int(200*ORIENT_TO_DEG)
+    def turn_90_right(self):
+        deg_to_move = int(self.DEG_180_TURN*self.ORIENT_TO_DEG)
 
         try:
-            left_motor.set_position_relative(deg_to_move)
-            wait_for_motor(left_motor)
+            self.LEFT_MOTOR.set_position_relative(deg_to_move)
+            self.wait_for_motor(self.LEFT_MOTOR)
         except IOError as error:
             print(error)
 
 
     #Turn angle method. pass an angle and direction to turn
-
-    def turn_angle(left_motor: Motor, right_motor: Motor, angle_deg: int, direction: str):
-        deg_to_move = int(angle_deg*ORIENT_TO_DEG)
+    def turn_angle(self, angle_deg: int, direction: str):
+        deg_to_move = int(angle_deg*self.ORIENT_TO_DEG)
 
         try:
             if direction == "left":
-                right_motor.set_position_relative(deg_to_move)
-                wait_for_motor(right_motor)
+                self.RIGHT_MOTOR.set_position_relative(deg_to_move)
+                self.wait_for_motor(self.RIGHT_MOTOR)
 
             elif direction == "right":
-                left_motor.set_position_relative(deg_to_move)
-                wait_for_motor(left_motor)
+                self.LEFT_MOTOR.set_position_relative(deg_to_move)
+                self.wait_for_motor(self.LEFT_MOTOR)
         except IOError as error:
             print(error)
 
-    
+
+if __name__ == "__main__":
+    #Testing movement
+    wheels = Wheels("B", "C")
+    wheels.turn_90_left()
+    wheels.turn_90_left()
+    wheels.turn_90_right()
+    wheels.turn_90_right()
+    wheels.turn_180(True)
+    wheels.turn_180(False)
