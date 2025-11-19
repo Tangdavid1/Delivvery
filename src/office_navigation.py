ce12166e9bbd5610e0c4bde8f2c5157ffa5ef74d
ssabba7 <sarah.sabbagh@mail.mcgill.ca>
2025-11-19 14:46:18 -0500

    adding first final draft of office_navigation.py

--- src/office_navigation.py
+++ src/office_navigation.py
@@ -1,15 +1,18 @@
 from utils.brick import Motor, EV3ColorSensor
 
-import movement as mv
-import office_scanning as os
+import wheels as wh
+import office_scanningV2 as os
 import color_average_finder
 import color_detect
 import time 
 import math
 
-## Constants
-FORWARD_DISTANCE_CM = 120
-CORNER_DISTANCE_CM = 23
+## Constants to define: 92(23 * 3), 23, 69 (23*3), 46 (23*2), 5
+BLOCK_CM = 23
+OFFICE_ENTRANCE_DISTANCE_CM = 5
+
+#Counters
+office_entered = 0
 
 class NavigationSystem:
     def __init__(self, left_motor_port="B", right_motor_port="C", color_sensor_port=1):
@@ -17,73 +20,145 @@ class NavigationSystem:
         self.right_motor = Motor(right_motor_port)
         self.color_sensor = EV3ColorSensor(color_sensor_port)
 
-    def navigate_to_ll_office(self):
+    def enter_office(self):
+        """
+        Perform a 90-degree turn at a corner, and advance to the entrance of an office.
+        """
+        wh.turn_90_right(self)
+        wh.move_straight(OFFICE_ENTRANCE_DISTANCE_CM)
+
+    def exit_office(self):
+        """
+        Exit the office by reversing the entrance process.
+        """
+        wh.turn_90_left(self)
+
+    
+    def go_to_first_office(self):
         """
         Navigate to first office on the board (lower left office).
         """
+        office_entered += 1
+
         # Move forward towards the corner
-        mv.move_straight(FORWARD_DISTANCE_CM, self.left_motor, self.right_motor)
+        wh.go_straight(BLOCK_CM * 4)
 
-        # Turn at the corner
-        mv.turn_angle(90, self.left_motor, self.right_motor)
+        # Turn the corner and advance towards the entrance
+        wh.turn_90_right(self)
 
-        # Move towards the office
-        mv.move_straight(CORNER_DISTANCE_CM, self.left_motor, self.right_motor)
+        wh.go_straight(BLOCK_CM)
 
-        # Turn at the office entrance corner
-        mv.turn_angle(90, self.left_motor, self.right_motor)
-
-        # Move into the office
-        #### WAIT BUT IS IT 23 CM ALSO TO GET INTO THE OFFICE
-        mv.move_straight(CORNER_DISTANCE_CM, self.left_motor, self.right_motor)
+        self.enter_office()
 
         # Detect color to confirm arrival at office, for debugging
         detected_color = color_detect.detect_color(self.color_sensor)
         print(f"Arrived at office with color: {detected_color}")
 
-    def move_out_of_office(self):
-        """
-        Move out of the office to the hallway, assuming the robot is set to some reset position 
-        """
-        # Reset any orientation changes made inside the office, so that the robot faces the exit facing forward
-        os.reset_positon()
+        self.scanOffice(Motor)  
+
+        self.exit_office()
+
+       
+    def go_to_office2(self):
+
+        '''
+        Navigate to second office on the board.
+        '''
+        office_entered += 1
         
-        # Move back from the office (move 23 cm backwards from entrance)
-        mv.move_straight(-CORNER_DISTANCE_CM, self.left_motor, self.right_motor)
+        wh.go_straight(BLOCK_CM * 3)
 
-        # Turn at the corner
-        mv.turn_angle(90, self.left_motor, self.right_motor)
+        wh.turn_90_right(self)
 
-        # After turn, advance to next office, before turning the next corner
-        mv.move_straight(FORWARD_DISTANCE_CM - CORNER_DISTANCE_CM, self.left_motor, self.right_motor)
+        wh.go_straight(BLOCK_CM)
 
-        # Turn towards the hallway
-        mv.turn_angle(90, self.left_motor, self.right_motor)
+        wh.enter_office(self)
 
-    def navigate_to_next_office(self):
+        self.scanOffice(Motor)
+
+        if (self.count == 2) :
+            self.return_to_mailroom_from_office2()
+        else:
+            self.exit_office()    
+
+    def go_to_office3(self):
         '''
-        Navigate to the next office on the upper right side.
+        Navigate to third office on the board.
         '''
-        mv.move_straight(CORNER_DISTANCE_CM, self.left_motor, self.right_motor)
+        office_entered += 1
 
-        mv.turn_angle(90, self.left_motor, self.right_motor)
+        wh.go_straight(BLOCK_CM * 3)
 
-        mv.move_straight(CORNER_DISTANCE_CM, self.left_motor, self.right_motor)
+        wh.turn_90_right(self)
 
-        # Turn towards the mailroom
-        mv.turn_angle(-90, self.left_motor, self.right_motor)
+        wh.go_straight(BLOCK_CM)
+
+        wh.enter_office(self)
+
+        self.scanOffice(Motor)
+
+        if (self.count == 2) :
+            self.return_to_mailroom_from_office3()
+        
+        self.exit_office()
+        
+
+    def go_to_office4(self):
+        '''
+        Navigate to fourth office on the board.
+        '''
+        office_entered += 1
+
+        wh.go_straight(BLOCK_CM * 2)
+      
+        wh.enter_office(self)
+
+        self.scanOffice(Motor)
+
+        self.return_to_mailroom_from_office4()
+
+    def return_to_mailroom_from_office2(self):
+        '''
+        Return to mailroom after delivering at office 2.
+        '''
+        wh.turn_90_right(self)
+
+        wh.go_straight(BLOCK_CM)
+
+        wh.turn_90_left(self)
+
+        wh.go_straight(BLOCK_CM * 2)
+
+        wh.turn_90_left(self)
+
+        wh.go_straight(BLOCK_CM * 2)
+
+    def return_to_mailroom_from_office3(self):
+        '''
+        Return to mailroom after delivering at office 3.
+        '''
+
+        wh.go_straight(BLOCK_CM)
+
+        wh.turn_90_right(self)
+
+        wh.go_straight(BLOCK_CM * 2)
+
+    def return_to_mailroom_from_office4(self):
+        '''
+        Return to mailroom after delivering at office 4.
+        '''
+        wh.turn_90_right(self)
+
+        wh.go_straight(BLOCK_CM)
+        
+        wh.turn_90_left(self)
+
+        wh.go_straight(BLOCK_CM * 2)
 
 
-    
-    def return_to_mailroom(self):
-        """
-        Navigate back to mailroom from office.
-        """
-        # Move back from the office
-        mv.move_straight(-CORNER_DISTANCE_CM, self.left_motor, self.right_motor)
 
-        # Turn back at the corner
-        mv.turn_angle(-90, self.left_motor, self.right_motor)
+        
 
-        # Move back to mailroom
-        mv.move_straight(-FORWARD_DISTANCE_CM, self.left_motor, self.right_motor)
+        
+        
